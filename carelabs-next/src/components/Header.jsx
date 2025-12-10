7e19b4709708dca0185550cdd9c5012141f18aa2
SreyasTest <test.sreyas@gmail.com>
2025-12-10 14:43:55 +0530

    add new  homePage collection 10

--- carelabs-next/src/components/Header.jsx
+++ carelabs-next/src/components/Header.jsx
@@ -144,7 +144,7 @@ const Header = () => {
                 width={400}
                 height={100}
                 priority   
-                loading="eager"           />
+                fetchPriority="high"     />
             </Link>
           </div>
 
