9a46f23f44517f7639deea5c49db7cddb60a3865
SreyasTest <test.sreyas@gmail.com>
2025-12-10 14:26:10 +0530

    add new  homePage collection8

--- carelabs-next/src/components/Header.jsx
+++ carelabs-next/src/components/Header.jsx
@@ -137,15 +137,13 @@ const Header = () => {
         <div className="header w-full  h-full flex items-center justify-between lg:justify-center  2xl:p-3 ">
           <div className="logo flex item center justify-center w-[50%] sm:w-[40%]  lg:w-[22%] ">
             <Link href={currentLocale ? `/${currentLocale}/` : "/"}>
-              <img
+              <Image
                 className="p-3 md:w-[60%] lg:w-[75%] object-contain"
                 src={navbarData.Logo?.url}
                 alt="Logo"
                 width={400}
                 height={100}
-                loading="eager"
-                fetchPriority="high"
-              />
+                priority              />
             </Link>
           </div>
 
